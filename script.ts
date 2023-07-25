4ff0d8f75b854082a80d3653582ac10039f6d16d
Matheus Holanda <matheusholanda0997@gmail.com>
2023-07-25 14:08:25 -0300

    first commit

---
+++ script.ts
@@ -0,0 +1,15 @@
+const buttonSignUp = document.getElementById('signup')!;
+const divLeftSide = document.getElementById('left-side')!;
+const divRigthSide = document.getElementById('right-side')!;
+const divSignupContainer = document.getElementById('signup-container')!;
+const pAccount = document.querySelector('.p-account')!;
+
+buttonSignUp.addEventListener('click', () => {
+    divLeftSide.classList.add('active');
+    divRigthSide.classList.add('active');
+    divSignupContainer.classList.remove('active');
+});
+
+pAccount.addEventListener('click', () => {
+    window.location.href = 'index.html'
+});
